1e306b343253f9d29fd78729092af5b28f06ff4b
srv-nucleusteq <117688191+srv-nucleusteq@users.noreply.github.com>
2023-06-26 00:35:35 -0700

    Workflow_Test_26_26_23 project is uploaded on github

--- Workflow_Test_26_26_23/wf_WMS_To_SCDS_Every_4_Hrs_147/wf_WMS_To_SCDS_Every_4_Hrs.py
+++ Workflow_Test_26_26_23/wf_WMS_To_SCDS_Every_4_Hrs_147/wf_WMS_To_SCDS_Every_4_Hrs.py
@@ -1,3 +1,4 @@
 # Databricks notebook source
 
-# COMMAND ----------dbutils.notebook.run("./WorkflowExecutor", 0, {"workflowName":"wf_WMS_To_SCDS_Every_4_Hrs"})
+# COMMAND ----------
+dbutils.notebook.run("./WorkflowExecutor", 0, {"elementName":"wf_WMS_To_SCDS_Every_4_Hrs"})
