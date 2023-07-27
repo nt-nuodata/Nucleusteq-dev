e814c93ae73f270d9f9a1cb7436efde8db353a61
srv-nucleusteq <117688191+srv-nucleusteq@users.noreply.github.com>
2023-07-27 01:36:00 -0700

    Workflow_Structure_Change project is uploaded on github

---
+++ Workflow_Structure_Change/wf_WMS_To_SCDS_Every_4_Hrs_219/mappings/m_WM_E_Dept_PRE.py
@@ -0,0 +1,167 @@
+# Databricks notebook source
+# MAGIC %run "./udf_informatica"
+
+# COMMAND ----------
+
+
+from pyspark.sql.types import *
+
+spark.sql("use DELTA_TRAINING")
+spark.sql("set spark.sql.legacy.timeParserPolicy = LEGACY")
+
+# COMMAND ----------
+%run ./MappingUtility
+
+# COMMAND ----------
+mainWorkflowId = dbutils.widgets.get("mainWorkflowId")
+mainWorkflowRunId = dbutils.widgets.get("mainWorkflowRunId")
+parentName = dbutils.widgets.get("parentName")
+preVariableAssignment = dbutils.widgets.get("preVariableAssignment")
+postVariableAssignment = dbutils.widgets.get("postVariableAssignment")
+truncTargetTableOptions = dbutils.widgets.get("truncTargetTableOptions")
+variablesTableName = dbutils.widgets.get("variablesTableName")
+
+# COMMAND ----------
+#Truncate Target Tables
+truncateTargetTables(truncTargetTableOptions)
+
+# COMMAND ----------
+#Pre presession variable updation
+updateVariable(preVariableAssignment, variablesTableName, mainWorkflowId, parentName, "m_WM_E_Dept_PRE")
+
+# COMMAND ----------
+fetchAndCreateVariables(parentName,"m_WM_E_Dept_PRE", variablesTableName, mainWorkflowId)
+
+# COMMAND ----------
+# DBTITLE 1, Shortcut_to_E_DEPT_v2_0
+
+
+query_0 = f"""SELECT
+  DEPT_ID_v2 AS DEPT_ID_v2,
+  DEPT_CODE AS DEPT_CODE,
+  DESCRIPTION AS DESCRIPTION,
+  CREATE_DATE_TIME AS CREATE_DATE_TIME,
+  MOD_DATE_TIME AS MOD_DATE_TIME,
+  USER_ID AS USER_ID,
+  WHSE AS WHSE,
+  MISC_TXT_1 AS MISC_TXT_1,
+  MISC_TXT_2 AS MISC_TXT_2,
+  MISC_NUM_1 AS MISC_NUM_1,
+  MISC_NUM_2 AS MISC_NUM_2,
+  PERF_GOAL AS PERF_GOAL,
+  VERSION_ID AS VERSION_ID,
+  CREATED_DTTM AS CREATED_DTTM,
+  LAST_UPDATED_DTTM AS LAST_UPDATED_DTTM
+FROM
+  E_DEPT_v2"""
+
+df_0 = spark.sql(query_0)
+
+df_0.createOrReplaceTempView("Shortcut_to_E_DEPT_v2_0")
+
+# COMMAND ----------
+# DBTITLE 1, SQ_Shortcut_to_E_DEPT_1
+
+
+query_1 = f"""SELECT
+  DEPT_ID_v2 AS DEPT_ID,
+  DEPT_CODE AS DEPT_CODE,
+  DESCRIPTION AS DESCRIPTION,
+  CREATE_DATE_TIME AS CREATE_DATE_TIME,
+  MOD_DATE_TIME AS MOD_DATE_TIME,
+  USER_ID AS USER_ID,
+  WHSE AS WHSE,
+  MISC_TXT_1 AS MISC_TXT_1,
+  MISC_TXT_2 AS MISC_TXT_2,
+  MISC_NUM_1 AS MISC_NUM_1,
+  MISC_NUM_2 AS MISC_NUM_2,
+  PERF_GOAL AS PERF_GOAL,
+  VERSION_ID AS VERSION_ID,
+  CREATED_DTTM AS CREATED_DTTM,
+  LAST_UPDATED_DTTM AS LAST_UPDATED_DTTM,
+  monotonically_increasing_id() AS Monotonically_Increasing_Id
+FROM
+  Shortcut_to_E_DEPT_v2_0
+WHERE
+  {Initial_Load} (
+    trunc(CREATE_DATE_TIME) >= trunc(to_date('{Prev_Run_Dt}', 'MM/DD/YYYY HH24:MI:SS')) -1
+  )
+  OR (
+    trunc(MOD_DATE_TIME) >= trunc(to_date('{Prev_Run_Dt}', 'MM/DD/YYYY HH24:MI:SS')) -1
+  )
+  OR (
+    trunc(CREATED_DTTM) >= trunc(to_date('{Prev_Run_Dt}', 'MM/DD/YYYY HH24:MI:SS')) -1
+  )
+  OR (
+    trunc(LAST_UPDATED_DTTM) >= trunc(to_date('{Prev_Run_Dt}', 'MM/DD/YYYY HH24:MI:SS')) -1
+  )
+  AND 1 = 1"""
+
+df_1 = spark.sql(query_1)
+
+df_1.createOrReplaceTempView("SQ_Shortcut_to_E_DEPT_1")
+
+# COMMAND ----------
+# DBTITLE 1, EXPTRANS_2
+
+
+query_2 = f"""SELECT
+  {DC_NBR} AS DC_NBR_EXP,
+  DEPT_ID AS DEPT_ID,
+  DEPT_CODE AS DEPT_CODE,
+  DESCRIPTION AS DESCRIPTION,
+  CREATE_DATE_TIME AS CREATE_DATE_TIME,
+  MOD_DATE_TIME AS MOD_DATE_TIME,
+  USER_ID AS USER_ID,
+  WHSE AS WHSE,
+  MISC_TXT_1 AS MISC_TXT_1,
+  MISC_TXT_2 AS MISC_TXT_2,
+  MISC_NUM_1 AS MISC_NUM_1,
+  MISC_NUM_2 AS MISC_NUM_2,
+  PERF_GOAL AS PERF_GOAL,
+  VERSION_ID AS VERSION_ID,
+  CREATED_DTTM AS CREATED_DTTM,
+  LAST_UPDATED_DTTM AS LAST_UPDATED_DTTM,
+  SYSTIMESTAMP() AS LOAD_TSTMP_EXP,
+  Monotonically_Increasing_Id AS Monotonically_Increasing_Id
+FROM
+  SQ_Shortcut_to_E_DEPT_1"""
+
+df_2 = spark.sql(query_2)
+
+df_2.createOrReplaceTempView("EXPTRANS_2")
+
+# COMMAND ----------
+# DBTITLE 1, WM_E_DEPT_PRE
+
+
+spark.sql("""INSERT INTO
+  WM_E_DEPT_PRE
+SELECT
+  DC_NBR_EXP AS DC_NBR,
+  DEPT_ID AS DEPT_ID,
+  DEPT_CODE AS DEPT_CODE,
+  DESCRIPTION AS DESCRIPTION,
+  CREATE_DATE_TIME AS CREATE_DATE_TIME,
+  MOD_DATE_TIME AS MOD_DATE_TIME,
+  USER_ID AS USER_ID,
+  WHSE AS WHSE,
+  MISC_TXT_1 AS MISC_TXT_1,
+  MISC_TXT_2 AS MISC_TXT_2,
+  MISC_NUM_1 AS MISC_NUM_1,
+  MISC_NUM_2 AS MISC_NUM_2,
+  PERF_GOAL AS PERF_GOAL,
+  VERSION_ID AS VERSION_ID,
+  CREATED_DTTM AS CREATED_DTTM,
+  LAST_UPDATED_DTTM AS LAST_UPDATED_DTTM,
+  LOAD_TSTMP_EXP AS LOAD_TSTMP
+FROM
+  EXPTRANS_2""")
+
+# COMMAND ----------
+#Post session variable updation
+updateVariable(postVariableAssignment, variablesTableName, mainWorkflowId, parentName, "m_WM_E_Dept_PRE")
+
+# COMMAND ----------
+#Update Mapping Variables in database.
+persistVariables(variablesTableName, "m_WM_E_Dept_PRE", mainWorkflowId, parentName)
